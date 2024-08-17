8473c488f294bdb9f735efaeda615ca8bbb6f89e
Nouman Arshad <noumanarshad15926@gmail.com>
2024-08-17 19:43:56 +0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://replit.com/@noumanarshad159/AI-commerce-backend-1',
     withCredentials: true,
 });
 
