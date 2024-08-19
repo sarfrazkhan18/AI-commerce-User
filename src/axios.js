b2ec183b526272bf3031544418eff2ba45e1ef55
Nouman Arshad <noumanarshad15926@gmail.com>
2024-08-19 15:11:29 +0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://6459c1e4-7ca2-4b60-b590-2ca4bb2deb7e-00-h2ik2yni0ke7.worf.replit.dev',
-    withCredentials: true,
-    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Credentials': true
+    },
+    // withCredentials: true,
+    // credentials: 'include',
 });
 
 export default axiosInstance;
