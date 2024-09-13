bc3c06f789164c312210be4c7942887996710101
Nouman159 <noumanarshad15926@gmail.com>
2024-09-13 12:15:23 +0500

    Basr URL Chnage

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,8 @@ import Cookies from 'js-cookie';
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:5000',
     // baseURL: 'https://ai-commerce-backend-1-sarfrazkhan18.replit.app',
-    baseURL: 'https://d562108f-32d3-4317-8500-ec1af97cd89d-00-3ljhkv9buvdbe.kirk.replit.dev/',
+    baseURL: 'https://ai-commerce-backend-2-sarfrazkhan18.replit.app',
+    // baseURL: 'https://d562108f-32d3-4317-8500-ec1af97cd89d-00-3ljhkv9buvdbe.kirk.replit.dev/',
 
     withCredentials: true,
 });
